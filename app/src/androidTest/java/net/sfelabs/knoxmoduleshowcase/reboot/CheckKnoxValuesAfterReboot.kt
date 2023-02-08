a091f677eec30fa45002e3d53d491a6afc43316f
Jeff <jeff.picklyk@sea.samsung.com>
2023-02-08 13:53:40 -0500

    Added WLAN MTU settings API use cases

---
+++ app/src/androidTest/java/net/sfelabs/knoxmoduleshowcase/reboot/CheckKnoxValuesAfterReboot.kt
@@ -0,0 +1,20 @@
+package net.sfelabs.knoxmoduleshowcase.reboot
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import kotlinx.coroutines.test.runTest
+import net.sfelabs.common.core.ApiCall
+import net.sfelabs.knox_tactical.domain.use_cases.tactical.mtu.GetCustomMtuUseCase
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class CheckKnoxValuesAfterReboot {
+    private val sm = net.sfelabs.knox_tactical.di.KnoxModule.provideKnoxSystemManager()
+
+    @Test
+    fun getWlanMtu() = runTest {
+        val useCase = GetCustomMtuUseCase(sm)
+        val result = useCase.invoke()
+        assert(result is ApiCall.Success && result.data == 1430)
+    }
+}
---
+++ app/src/androidTest/java/net/sfelabs/knoxmoduleshowcase/reboot/SetKnoxValuesBeforeReboot.kt
@@ -0,0 +1,42 @@
+package net.sfelabs.knoxmoduleshowcase.reboot
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import kotlinx.coroutines.test.runTest
+import net.sfelabs.common.core.ApiCall
+import net.sfelabs.knox_tactical.domain.use_cases.tactical.mtu.GetCustomMtuUseCase
+import net.sfelabs.knox_tactical.domain.use_cases.tactical.mtu.SetCustomMtuUseCase
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class SetKnoxValuesBeforeReboot {
+    private val sm = net.sfelabs.knox_tactical.di.KnoxModule.provideKnoxSystemManager()
+    private val mtuValue = 1430
+    @Test
+    fun setWlanMtu() = runTest {
+        val useCase = SetCustomMtuUseCase(sm)
+        val result = useCase.invoke(mtuValue)
+        assert(result is ApiCall.Success)
+    }
+
+    @Test
+    fun checkWlanMtu() = runTest {
+        val useCase = GetCustomMtuUseCase(sm)
+        val result = useCase.invoke()
+        assert(result is ApiCall.Success && result.data == mtuValue)
+    }
+
+    /*
+    * There doesn't appear to be any way to run the adb shell commands from tests
+    @Test
+    fun checkWlanMtuFromAdb() = runTest {
+        val result = Runtime.getRuntime().exec("shell ip address show wlan0")
+        result.waitFor()
+        val stdOut = result.inputStream.bufferedReader().use(BufferedReader::readText)
+        val stdError = result.errorStream.bufferedReader().use(BufferedReader::readText)
+        println(stdOut)
+        println(stdError)
+    }
+     */
+
+}
