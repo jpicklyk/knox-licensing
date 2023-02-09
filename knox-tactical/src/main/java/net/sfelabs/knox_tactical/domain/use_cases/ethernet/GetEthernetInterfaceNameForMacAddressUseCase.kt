6150b61833262cc0d7c410d0bfd6f883e43ce1ad
Jeff <jeff.picklyk@sea.samsung.com>
2023-02-09 16:34:13 -0500

    New Knox APIs for this MR

---
+++ knox-tactical/src/main/java/net/sfelabs/knox_tactical/domain/use_cases/ethernet/GetEthernetInterfaceNameForMacAddressUseCase.kt
@@ -0,0 +1,29 @@
+package net.sfelabs.knox_tactical.domain.use_cases.ethernet
+
+import com.samsung.android.knox.custom.SystemManager
+import net.sfelabs.common.core.ApiCall
+import net.sfelabs.common.core.ui.UiText
+import net.sfelabs.knox_tactical.di.TacticalSdk
+import javax.inject.Inject
+
+class GetEthernetInterfaceNameForMacAddressUseCase @Inject constructor(
+    @TacticalSdk private val systemManager: SystemManager
+) {
+
+    operator fun invoke(macAddress: String): ApiCall<String> {
+        return try {
+            val result = systemManager.getEthernetInterfaceNameForMacAddress(macAddress)
+            if(result == null) {
+                ApiCall.Error(UiText.DynamicString("MAC Address doesn't exist"))
+            } else {
+                ApiCall.Success(result)
+            }
+
+        } catch (e: Exception) {
+            ApiCall.Error(
+                UiText.DynamicString(
+                    e.message!!
+                ))
+        }
+    }
+}
---
+++ knox-tactical/src/main/java/net/sfelabs/knox_tactical/domain/use_cases/ethernet/GetMacAddressForInterfaceUseCase.kt
@@ -0,0 +1,29 @@
+package net.sfelabs.knox_tactical.domain.use_cases.ethernet
+
+import com.samsung.android.knox.custom.SystemManager
+import net.sfelabs.common.core.ApiCall
+import net.sfelabs.common.core.ui.UiText
+import net.sfelabs.knox_tactical.di.TacticalSdk
+import javax.inject.Inject
+
+class GetMacAddressForInterfaceUseCase @Inject constructor(
+    @TacticalSdk private val systemManager: SystemManager
+) {
+
+    operator fun invoke(interfaceName: String): ApiCall<String> {
+        return try {
+            val result = systemManager.getMacAddressForEthernetInterface(interfaceName)
+            if(result == null) {
+                ApiCall.Error(UiText.DynamicString("Ethernet interface doesn't exist"))
+            } else {
+                ApiCall.Success(result)
+            }
+
+        } catch (e: Exception) {
+            ApiCall.Error(
+                UiText.DynamicString(
+                    e.message!!
+                ))
+        }
+    }
+}
