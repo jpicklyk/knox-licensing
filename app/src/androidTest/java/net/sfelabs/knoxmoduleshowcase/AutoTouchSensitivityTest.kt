afa5c1eab0d3a53dae8c5e482303f8e0154da64e
Jeff <jeff.picklyk@sea.samsung.com>
2023-06-05 11:07:44 -0400

    Updated test case to also disable the auto touch sensitivity

--- app/src/androidTest/java/net/sfelabs/knoxmoduleshowcase/AutoTouchSensitivityTest.kt
+++ app/src/androidTest/java/net/sfelabs/knoxmoduleshowcase/AutoTouchSensitivityTest.kt
@@ -1,33 +1,43 @@
 package net.sfelabs.knoxmoduleshowcase
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import net.sfelabs.common.core.ApiCall
 import net.sfelabs.knox_tactical.di.KnoxModule
+import net.sfelabs.knox_tactical.domain.use_cases.auto_touch.GetAutoTouchSensitivityUseCase
+import net.sfelabs.knox_tactical.domain.use_cases.auto_touch.SetAutoTouchSensitivityUseCase
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class AutoTouchSensitivityTest {
     private val sm = KnoxModule.provideKnoxSettingsManager()
-    @OptIn(ExperimentalCoroutinesApi::class)
+
     @Test
     fun enableAutoTouchSensitivity() = runTest {
-        val setUseCase =
-            net.sfelabs.knox_tactical.domain.use_cases.auto_touch.SetAutoTouchSensitivityUseCase(
-                sm
-            )
-        val getUseCase =
-            net.sfelabs.knox_tactical.domain.use_cases.auto_touch.GetAutoTouchSensitivityUseCase(
-                sm
-            )
+        val setUseCase = SetAutoTouchSensitivityUseCase(sm)
+        val getUseCase = GetAutoTouchSensitivityUseCase(sm)
         assert(setUseCase(true) is ApiCall.Success)
         val result = getUseCase.invoke()
         assert(result is ApiCall.Success)
         when(result) {
             is ApiCall.Success -> {
                 assert(result.data)
+            }
+            else -> assert(false)
+        }
+    }
+
+    @Test
+    fun disableAutoTouchSensitivity() = runTest {
+        val setUseCase = SetAutoTouchSensitivityUseCase(sm)
+        val getUseCase = GetAutoTouchSensitivityUseCase(sm)
+        assert(setUseCase(false) is ApiCall.Success)
+        val result = getUseCase.invoke()
+        assert(result is ApiCall.Success)
+        when(result) {
+            is ApiCall.Success -> {
+                assert(!result.data)
             }
             else -> assert(false)
         }
