5e5399ac265dbe0044229ddb815aba13f42aa122
Jeff <jeff.picklyk@sea.samsung.com>
2023-06-06 15:02:16 -0400

    Call control get use cases

---
+++ knox-tactical/src/main/java/net/sfelabs/knox_tactical/domain/use_cases/calling/GetAutoCallPickupStateUseCase.kt
@@ -0,0 +1,32 @@
+package net.sfelabs.knox_tactical.domain.use_cases.calling
+
+import com.samsung.android.knox.custom.SystemManager
+import kotlinx.coroutines.coroutineScope
+import net.sfelabs.common.core.ApiCall
+import net.sfelabs.common.core.ui.UiText
+import net.sfelabs.knox_tactical.di.TacticalSdk
+import net.sfelabs.knox_tactical.domain.model.AutoCallPickupState
+import javax.inject.Inject
+
+/**
+ * This Knox API isn't TE specific but the flag ENABLED_ALWAYS_ACCEPT is.
+ */
+class GetAutoCallPickupStateUseCase @Inject constructor(
+    @TacticalSdk private val systemManager: SystemManager
+){
+    suspend operator fun invoke(): ApiCall<AutoCallPickupState> {
+        return coroutineScope {
+            try {
+                ApiCall.Success(AutoCallPickupState.invoke(systemManager.autoCallPickupState))
+            } catch (e: SecurityException) {
+                ApiCall.Error(
+                    UiText.DynamicString(
+                        "The use of this API requires the caller to have the " +
+                                "\"com.samsung.android.knox.permission.KNOX_CUSTOM_SYSTEM\" permission"
+                    ))
+            } catch (nsm: NoSuchMethodError) {
+                ApiCall.NotSupported
+            }
+        }
+    }
+}
