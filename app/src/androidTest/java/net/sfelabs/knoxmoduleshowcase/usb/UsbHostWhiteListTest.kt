6b64df3c2e9147b5005cb238cf0b63c5a5ded9dd
Jeff <jeff.picklyk@sea.samsung.com>
2023-06-06 14:55:24 -0400

    USB Host Mode controls and tests

--- app/src/androidTest/java/net/sfelabs/knoxmoduleshowcase/usb/UsbHostWhiteListTest.kt
+++ app/src/androidTest/java/net/sfelabs/knoxmoduleshowcase/usb/UsbHostWhiteListTest.kt
@@ -4,10 +4,14 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import com.samsung.android.knox.AppIdentity
 import com.samsung.android.knox.EnterpriseDeviceManager
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import net.sfelabs.common.core.ApiCall
 import net.sfelabs.common.core.signing.getApplicationSignatures
+import net.sfelabs.knox_common.AllowUsbHostStorageUseCase
+import net.sfelabs.knox_tactical.domain.use_cases.usb.AddPackageToUsbWhiteListUseCase
+import net.sfelabs.knox_tactical.domain.use_cases.usb.GetPackagesFromUsbHostWhiteListUseCase
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -24,26 +28,41 @@ import org.junit.runner.RunWith
  * 8. check device A is able to read storage of the other device(B) by selecting "MTP Host" application.
  *   --> possible
  */
-@OptIn(ExperimentalCoroutinesApi::class)
 @RunWith(AndroidJUnit4::class)
 class UsbHostWhiteListTest {
     private val appContext = InstrumentationRegistry.getInstrumentation().targetContext
     private val edm: EnterpriseDeviceManager = EnterpriseDeviceManager.getInstance(appContext)
 
+
+    @Before
+    fun disableUsbHostMode()= runTest {
+        val hostStorageUseCase = AllowUsbHostStorageUseCase(edm.restrictionPolicy)
+        val res = hostStorageUseCase.invoke(false)
+        assert(res is ApiCall.Success)
+    }
+
     @Test
-    fun testAddPackageToUsbWhitelist() = runTest {
-        val useCase =
-            net.sfelabs.knox_tactical.domain.use_cases.usb.AddPackageToUsbWhiteListUseCase(
-                edm
-            )
-        val packageName = "com.android.chrome"
+    fun addPackageToUsbWhitelist() = runTest {
+        val useCase = AddPackageToUsbWhiteListUseCase(edm)
+        val packageName = "com.android.mtp"
         val signatures = getApplicationSignatures(packageName, appContext)
         val sig = signatures[0]
         println("Signature: $sig")
         val appIdentity = AppIdentity(packageName, sig)
         val result = useCase.invoke(appIdentity)
         assert(result is ApiCall.Success)
+
+        val useCase2 = GetPackagesFromUsbHostWhiteListUseCase(edm)
+        val result2 =  useCase2.invoke()
+        assert(result2 is ApiCall.Success && result2.data.contains("com.android.mtp"))
+
     }
 
 
+
+    @After
+    fun allowUsbHostTest() = runTest {
+        val hostStorageUseCase = AllowUsbHostStorageUseCase(edm.restrictionPolicy)
+        val res = hostStorageUseCase.invoke(true)
+    }
 }
---
+++ knox-common/src/main/java/net/sfelabs/knox_common/AllowUsbHostStorageUseCase.kt
@@ -0,0 +1,33 @@
+package net.sfelabs.knox_common
+
+import com.samsung.android.knox.restriction.RestrictionPolicy
+import kotlinx.coroutines.coroutineScope
+import net.sfelabs.common.core.ApiCall
+import net.sfelabs.common.core.UnitApiCall
+import net.sfelabs.common.core.ui.UiText
+import javax.inject.Inject
+
+class AllowUsbHostStorageUseCase @Inject constructor(
+    private val restrictionPolicy: RestrictionPolicy
+) {
+    suspend operator fun invoke(allow: Boolean): UnitApiCall {
+        return coroutineScope {
+            try {
+                val result = restrictionPolicy.allowUsbHostStorage(allow)
+                if (result)
+                    ApiCall.Success(Unit)
+                else
+                    ApiCall.Error(UiText.DynamicString("The API allowUsbHostStorage($allow) failed"))
+            } catch(se: SecurityException) {
+                ApiCall.Error(
+                    UiText.DynamicString(
+                    "The use of this API requires the caller to have the " +
+                            "\"com.samsung.android.knox.permission.KNOX_RESTRICTION_MGMT\" " +
+                            "permission which has a protection level of signature."
+                ))
+            }catch (ex: NoSuchMethodException) {
+                ApiCall.NotSupported
+            }
+        }
+    }
+}
