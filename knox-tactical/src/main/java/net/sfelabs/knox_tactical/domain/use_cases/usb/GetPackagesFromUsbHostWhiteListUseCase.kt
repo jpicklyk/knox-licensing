bf23d7aa045925f3595dfa85d78f263d370841b3
Jeff <jeff.picklyk@sea.samsung.com>
2023-06-06 15:04:38 -0400

    USB host whitelisting use case

---
+++ knox-tactical/src/main/java/net/sfelabs/knox_tactical/domain/use_cases/usb/GetPackagesFromUsbHostWhiteListUseCase.kt
@@ -0,0 +1,28 @@
+package net.sfelabs.knox_tactical.domain.use_cases.usb
+
+import com.samsung.android.knox.EnterpriseDeviceManager
+import com.samsung.android.knox.application.ApplicationPolicy
+import kotlinx.coroutines.coroutineScope
+import net.sfelabs.common.core.ApiCall
+import net.sfelabs.common.core.ui.UiText
+import net.sfelabs.knox_tactical.di.TacticalSdk
+import javax.inject.Inject
+
+class GetPackagesFromUsbHostWhiteListUseCase @Inject constructor(
+    @TacticalSdk private val enterpriseDeviceManager: EnterpriseDeviceManager
+) {
+    suspend operator fun invoke(): ApiCall<List<String>> {
+        return coroutineScope {
+            try {
+                val appPolicy: ApplicationPolicy = enterpriseDeviceManager.applicationPolicy
+                ApiCall.Success(appPolicy.packagesFromUsbHostWhiteList)
+
+            } catch (e: Exception) {
+                ApiCall.Error(
+                    UiText.DynamicString(
+                        e.message!!
+                    ))
+            }
+        }
+    }
+}
