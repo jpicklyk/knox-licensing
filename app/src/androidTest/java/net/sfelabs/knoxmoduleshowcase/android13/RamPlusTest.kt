1da8f7c963361dd90f76a4c4268fc15ea85dbd63
Jeff <jeff.picklyk@sea.samsung.com>
2023-06-20 14:08:53 -0400

    Allowed the test for checking that RAM plus

--- app/src/androidTest/java/net/sfelabs/knoxmoduleshowcase/android13/RamPlusTest.kt
+++ app/src/androidTest/java/net/sfelabs/knoxmoduleshowcase/android13/RamPlusTest.kt
@@ -1,10 +1,12 @@
 package net.sfelabs.knoxmoduleshowcase.android13
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
 import kotlinx.coroutines.test.runTest
 import net.sfelabs.common.core.ApiCall
 import net.sfelabs.knox_tactical.di.KnoxModule
 import net.sfelabs.knox_tactical.domain.use_cases.ramplus.GetRamPlusDisabledStateUseCase
+import net.sfelabs.knox_tactical.domain.use_cases.ramplus.SetRamPlusStateUseCase
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -13,16 +15,17 @@ class RamPlusTest {
     private val sm = KnoxModule.provideKnoxSystemManager()
 
     @Test
+    @SmallTest
     fun testDefaultDisabled() = runTest {
         val getUseCase = GetRamPlusDisabledStateUseCase(sm)
         val result2 = getUseCase.invoke()
         assert(result2 is ApiCall.Success && result2.data)
     }
-    /* Setting the state force restarts the device so we can't set and immediately read the state
+    // Setting the state force restarts the device so we can't set and immediately read the state
         @Test
         fun testDisableRamPlus() = runTest {
             val setUseCase = SetRamPlusStateUseCase(sm)
-            val getUseCase = GetRamPlusStateUseCase(sm)
+            val getUseCase = GetRamPlusDisabledStateUseCase(sm)
 
             val result = setUseCase.invoke(true)
             assert(result is ApiCall.Success)
@@ -34,7 +37,7 @@ class RamPlusTest {
         @Test
         fun testEnableRamPlus() = runTest {
             val setUseCase = SetRamPlusStateUseCase(sm)
-            val getUseCase = GetRamPlusStateUseCase(sm)
+            val getUseCase = GetRamPlusDisabledStateUseCase(sm)
 
             val result = setUseCase.invoke(false)
             assert(result is ApiCall.Success)
@@ -43,5 +46,4 @@ class RamPlusTest {
             assert(result2 is ApiCall.Success && result2.data)
         }
 
-     */
 }
