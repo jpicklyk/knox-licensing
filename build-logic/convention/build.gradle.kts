e58c3c239ee77582310decbb5994a6db1dab4455
Jeff <jeff.picklyk@sea.samsung.com>
2023-06-25 13:53:02 -0400

    Gradle plugins for better management of module configurations through convention.

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,54 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    `kotlin-dsl`
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+}
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.ksp.gradlePlugin)
+}
+
+gradlePlugin {
+    plugins {
+        register("androidApplicationCompose") {
+            id = "sfelabs.android.application.compose"
+            implementationClass = "AndroidApplicationComposeConventionPlugin"
+        }
+        register("androidApplication") {
+            id = "sfelabs.android.application"
+            implementationClass = "AndroidApplicationConventionPlugin"
+        }
+        register("androidHilt") {
+            id = "sfelabs.android.hilt"
+            implementationClass = "AndroidHiltConventionPlugin"
+        }
+        register("jvmLibrary") {
+            id = "sfelabs.jvm.library"
+            implementationClass = "JvmLibraryConventionPlugin"
+        }
+        register("androidTest") {
+            id = "sfelabs.android.test"
+            implementationClass = "AndroidTestConventionPlugin"
+        }
+        register("androidLibraryCompose") {
+            id = "sfelabs.android.library.compose"
+            implementationClass = "AndroidLibraryComposeConventionPlugin"
+        }
+        register("androidLibrary") {
+            id = "sfelabs.android.library"
+            implementationClass = "AndroidLibraryConventionPlugin"
+        }
+    }
+}
