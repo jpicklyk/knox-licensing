75881764c513629d719bcd640c9b160a0ed6b9c3
Jeff <jeff.picklyk@sea.samsung.com>
2023-08-18 14:37:18 -0400

    added use cases for ccmode and test cases

---
+++ knox-common/src/main/java/net/sfelabs/knox_common/domain/use_cases/GetCCModeUseCase.kt
@@ -0,0 +1,28 @@
+package net.sfelabs.knox_common.domain.use_cases
+
+import com.samsung.android.knox.EnterpriseKnoxManager
+import kotlinx.coroutines.coroutineScope
+import net.sfelabs.core.ui.ApiCall
+import net.sfelabs.core.ui.UiText
+import javax.inject.Inject
+
+class GetCCModeUseCase @Inject constructor(
+    private val enterpriseKnoxManager: EnterpriseKnoxManager
+) {
+
+    suspend operator fun invoke(): ApiCall<Int> {
+        val restrictionPolicy = enterpriseKnoxManager.advancedRestrictionPolicy
+        return coroutineScope {
+            try {
+                ApiCall.Success(restrictionPolicy.ccModeState)
+            } catch (se: SecurityException) {
+                ApiCall.Error(
+                    UiText.DynamicString(
+                        "The use of this API requires the caller to have the " +
+                                "\"com.samsung.android.knox.permission.KNOX_ADVANCED_RESTRICTION\" permission"
+                    )
+                )
+            }
+        }
+    }
+}
---
+++ knox-common/src/main/java/net/sfelabs/knox_common/domain/use_cases/SetCCModeUseCase.kt
@@ -0,0 +1,36 @@
+package net.sfelabs.knox_common.domain.use_cases
+
+import com.samsung.android.knox.EnterpriseKnoxManager
+import kotlinx.coroutines.coroutineScope
+import net.sfelabs.core.ui.ApiCall
+import net.sfelabs.core.ui.UiText
+import javax.inject.Inject
+
+class SetCCModeUseCase @Inject constructor(
+    private val enterpriseKnoxManager: EnterpriseKnoxManager
+) {
+
+    suspend operator fun invoke(enable: Boolean): ApiCall<Boolean> {
+        val restrictionPolicy = enterpriseKnoxManager.advancedRestrictionPolicy
+        return coroutineScope {
+            try {
+                when (val result = restrictionPolicy.setCCMode(enable)) {
+                    true -> {
+                        ApiCall.Success(data = enable)
+                    }
+
+                    false -> {
+                        ApiCall.Error(UiText.DynamicString("Failure occurred applying setCCMode(${enable})"))
+                    }
+                }
+            } catch (se: SecurityException) {
+                ApiCall.Error(
+                    UiText.DynamicString(
+                        "The use of this API requires the caller to have the " +
+                                "\"com.samsung.android.knox.permission.KNOX_ADVANCED_RESTRICTION\" permission"
+                    )
+                )
+            }
+        }
+    }
+}
