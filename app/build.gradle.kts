8ac82dce5d5d157caaaaec687c3055a1a9f6f5e7
Jeff <jeff.picklyk@sea.samsung.com>
2023-12-21 14:53:00 -0500

    Fixed an issue where the TacticalJUnitRunner was not applied at the application level for the testInstrumentationRunner.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         versionCode = 1
         versionName = "0.0.1"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = "net.sfelabs.knox_tactical.TacticalJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
         }
