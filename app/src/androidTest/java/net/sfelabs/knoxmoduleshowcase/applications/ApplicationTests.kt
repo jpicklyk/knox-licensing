de69695568d6d7d93d76f108c2d48adb036cd5fe
Jeff <jeff.picklyk@sea.samsung.com>
2024-01-31 21:56:35 -0500

    Added automated tests for application removal

---
+++ app/src/androidTest/java/net/sfelabs/knoxmoduleshowcase/applications/ApplicationTests.kt
@@ -0,0 +1,61 @@
+package net.sfelabs.knoxmoduleshowcase.applications
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import androidx.test.platform.app.InstrumentationRegistry
+import junit.framework.TestCase.assertFalse
+import net.sfelabs.core.domain.isPackageInstalled
+import net.sfelabs.knox_tactical.annotations.TacticalSdkSuppress
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class ApplicationTests {
+    private val pm = InstrumentationRegistry.getInstrumentation().targetContext.packageManager
+
+    @Test
+    @TacticalSdkSuppress(minReleaseVersion = 110)
+    fun kidsModeRemoved() {
+        assertFalse(isPackageInstalled("com.sec.android.app.kidshome", pm))
+        assertFalse(isPackageInstalled("com.sec.android.app.kidsinstaller", pm))
+    }
+
+    @Test
+    @TacticalSdkSuppress(minReleaseVersion = 130)
+    fun gameLauncherRemoved() {
+        assertFalse(isPackageInstalled("com.samsung.android.game.gamehome", pm))
+        assertFalse(isPackageInstalled("com.samsung.android.game.gametools", pm))
+        assertFalse(isPackageInstalled("com.samsung.android.game.gos", pm))
+    }
+
+    @Test
+    @TacticalSdkSuppress(minReleaseVersion = 110)
+    fun facebookRemoved() {
+        assertFalse(isPackageInstalled("com.facebook.android", pm))
+        //preload
+        assertFalse(isPackageInstalled("com.facebook.katana", pm))
+        assertFalse(isPackageInstalled("com.facebook.appmanager", pm))
+        assertFalse(isPackageInstalled("com.facebook.system", pm))
+        assertFalse(isPackageInstalled("com.facebook.services", pm))
+    }
+
+    @Test
+    @TacticalSdkSuppress(minReleaseVersion = 110)
+    fun netflixRemoved() {
+        assertFalse(isPackageInstalled("com.netflix.mediaclient", pm))
+    }
+
+    @Test
+    @TacticalSdkSuppress(minReleaseVersion = 110)
+    fun spotifyRemoved() {
+        assertFalse(isPackageInstalled("com.spotify.music", pm))
+    }
+
+    @Test
+    @TacticalSdkSuppress(minReleaseVersion = 110)
+    fun linkedInRemoved() {
+        assertFalse(isPackageInstalled("com.linkedin.android", pm))
+    }
+}
---
+++ core/common/src/main/java/net/sfelabs/core/domain/packageManagerUtils.kt
@@ -0,0 +1,12 @@
+package net.sfelabs.core.domain
+
+import android.content.pm.PackageManager
+
+fun isPackageInstalled(packageName: String, packageManager: PackageManager): Boolean {
+    return try {
+        packageManager.getPackageInfo(packageName, 0)
+        true
+    } catch (e: PackageManager.NameNotFoundException) {
+        false
+    }
+}
