e3c2ccb04eaf1295f0bb6b875e3a779481f75481
Jeff <jeff.picklyk@sea.samsung.com>
2024-02-21 11:38:33 -0500

    Removed KeyGenTest class from small tests.

--- app/src/androidTest/java/net/sfelabs/knoxmoduleshowcase/android/KeyGenTest.kt
+++ app/src/androidTest/java/net/sfelabs/knoxmoduleshowcase/android/KeyGenTest.kt
@@ -4,7 +4,6 @@ import android.app.admin.DevicePolicyManager
 import android.content.Context
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.FlakyTest
-import androidx.test.filters.SmallTest
 import androidx.test.platform.app.InstrumentationRegistry
 import com.samsung.android.knox.integrity.EnhancedAttestationPolicy
 import com.samsung.knox.attesation.blobvalidator.library.Verdict
@@ -21,7 +20,6 @@ import org.junit.runner.RunWith
 import java.util.UUID
 
 @RunWith(AndroidJUnit4::class)
-@SmallTest
 class KeyGenTest {
     private lateinit var context: Context
     private lateinit var attestationPolicy: EnhancedAttestationPolicy
