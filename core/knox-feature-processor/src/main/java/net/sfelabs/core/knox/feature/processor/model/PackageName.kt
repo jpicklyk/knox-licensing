cd04b4a8ff94bb2964989b1b6ddb716212f0a1f4
Jeff Picklyk <jpicklyk@gmail.com>
2024-12-10 13:55:54 -0500

    Cleaning up unnecessary entries.

--- core/knox-feature-processor/src/main/java/net/sfelabs/core/knox/feature/processor/model/PackageName.kt
+++ core/knox-feature-processor/src/main/java/net/sfelabs/core/knox/feature/processor/model/PackageName.kt
@@ -1,12 +1,10 @@
 package net.sfelabs.core.knox.feature.processor.model
 
 enum class PackageName(val value: String) {
-    FEATURE_DOMAIN("net.sfelabs.core.knox.feature.domain"),
     FEATURE_COMPONENT("net.sfelabs.core.knox.feature.domain.component"),
     FEATURE_MODEL("net.sfelabs.core.knox.feature.domain.model"),
     FEATURE_REGISTRY("net.sfelabs.core.knox.feature.domain.registry"),
     FEATURE_HANDLER("net.sfelabs.core.knox.feature.domain.handler"),
-    FEATURE_HILT("net.sfelabs.core.knox.feature.hilt"),
-    API_DOMAIN("net.sfelabs.core.knox.api.domain")
+    FEATURE_HILT("net.sfelabs.core.knox.feature.hilt")
 
 }
