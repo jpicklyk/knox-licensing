9ae91105ff019b9d5ce85da5037d2de0720b5a40
Jeff Picklyk <jpicklyk@gmail.com>
2024-12-19 21:35:17 -0500

    Using the new executeAndCombine function to make the code more readable

--- knox-tactical/src/main/java/net/sfelabs/knox_tactical/domain/use_cases/screen/GetNightVisionModeStateUseCase.kt
+++ knox-tactical/src/main/java/net/sfelabs/knox_tactical/domain/use_cases/screen/GetNightVisionModeStateUseCase.kt
@@ -2,29 +2,26 @@ package net.sfelabs.knox_tactical.domain.use_cases.screen
 
 import net.sfelabs.core.domain.repository.PreferencesRepository
 import net.sfelabs.core.knox.api.domain.ApiResult
+import net.sfelabs.core.knox.api.domain.ApiUseCaseExecutor
 import net.sfelabs.core.knox.api.domain.CoroutineApiUseCase
-import net.sfelabs.core.knox.api.domain.DefaultApiError
-import net.sfelabs.core.knox.api.domain.executeForSuccess
-import net.sfelabs.core.knox.api.domain.useCaseBlock
 import net.sfelabs.knox_tactical.domain.model.NightVisionState
 
 class GetNightVisionModeStateUseCase(
     private val preferenceRepository: PreferencesRepository = PreferencesRepository.getInstance()
 ): CoroutineApiUseCase<Unit, NightVisionState>() {
 
-    override suspend fun execute(params: Unit): ApiResult<NightVisionState> = useCaseBlock {
-        val isEnabled = executeForSuccess { GetNightVisionModeUseCase().invoke(Unit) }
-        val useRedOverlay = executeForSuccess {
-            GetNightVisionRedOverlayUseCase(preferenceRepository).invoke(Unit)
+    override suspend fun execute(params: Unit): ApiResult<NightVisionState> {
+        return ApiUseCaseExecutor().executeAndCombine(
+            operations = listOf(
+                { GetNightVisionModeUseCase().invoke(Unit) },
+                { GetNightVisionRedOverlayUseCase(preferenceRepository).invoke(Unit) }
+            ),
+            type = Boolean::class.java
+        ) { results ->
+            NightVisionState(
+                isEnabled = results[0],
+                useRedOverlay = results[1]
+            )
         }
-
-        if (isEnabled != null && useRedOverlay != null) {
-            ApiResult.Success(NightVisionState(
-                isEnabled = isEnabled,
-                useRedOverlay = useRedOverlay
-            ))
-        } else {
-            errors().firstOrNull() ?: ApiResult.Error(DefaultApiError.UnexpectedError())
-        }
-    }.getResults().filterIsInstance<ApiResult<NightVisionState>>().first()
+    }
 }
