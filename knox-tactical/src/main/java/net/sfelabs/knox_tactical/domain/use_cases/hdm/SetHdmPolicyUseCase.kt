847e3ec0ecc3750f75d95d9cc7a3c1566bda4ba7
Jeff Picklyk <jpicklyk@gmail.com>
2025-01-14 11:18:23 -0500

    Fixing the mapping of the return boolean to map to success or error results.

--- knox-tactical/src/main/java/net/sfelabs/knox_tactical/domain/use_cases/hdm/SetHdmPolicyUseCase.kt
+++ knox-tactical/src/main/java/net/sfelabs/knox_tactical/domain/use_cases/hdm/SetHdmPolicyUseCase.kt
@@ -4,17 +4,22 @@ import com.samsung.android.knox.EnterpriseDeviceManager
 import net.sfelabs.core.knox.android.WithAndroidApplicationContext
 import net.sfelabs.core.domain.usecase.model.ApiResult
 import net.sfelabs.core.domain.usecase.base.SuspendingUseCase
+import net.sfelabs.core.domain.usecase.model.DefaultApiError
 
-class SetHdmPolicyUseCase: WithAndroidApplicationContext, SuspendingUseCase<SetHdmPolicyUseCase.Params, Boolean>() {
+class SetHdmPolicyUseCase: WithAndroidApplicationContext, SuspendingUseCase<SetHdmPolicyUseCase.Params, Unit>() {
     data class Params(val policy: Int, val reboot: Boolean = false)
 
-    suspend operator fun invoke(policy: Int, reboot: Boolean): ApiResult<Boolean> {
+    suspend operator fun invoke(policy: Int, reboot: Boolean): ApiResult<Unit> {
         return invoke(Params(policy, reboot))
     }
 
-    override suspend fun execute(params: Params): ApiResult<Boolean> {
+    override suspend fun execute(params: Params): ApiResult<Unit> {
         val hdmManager =
             EnterpriseDeviceManager.getInstance(applicationContext).hypervisorDeviceManager
-        return ApiResult.Success(hdmManager.stealthHwControl(params.policy, params.reboot))
+
+        return when (hdmManager.stealthHwControl(params.policy, params.reboot)) {
+            true -> ApiResult.Success(Unit)
+            false -> ApiResult.Error(DefaultApiError.UnexpectedError("HDM Policy failed to apply"))
+        }
     }
 }
