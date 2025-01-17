5be759367fcbb2ad2a8abed13bd7bcebd13d21d9
Jeff Picklyk <jpicklyk@gmail.com>
2025-01-17 17:40:23 -0500

    Renamed FeatureUiState class to PolicyUiState and updated its references.

--- core/knox-feature-ui/src/main/java/net/sfelabs/core/knox/feature/ui/model/PolicyUiState.kt
+++ core/knox-feature-ui/src/main/java/net/sfelabs/core/knox/feature/ui/model/PolicyUiState.kt
@@ -1,31 +1,32 @@
 package net.sfelabs.core.knox.feature.ui.model
 
-sealed class FeatureUiState {
+sealed class PolicyUiState {
     abstract val isSupported: Boolean
     abstract val title: String
     abstract val featureName: String
     abstract val description: String
     abstract val isLoading: Boolean
     abstract val error: String?
+    abstract val isEnabled: Boolean
 
     data class Toggle(
         override val isSupported: Boolean,
         override val title: String,
         override val featureName: String,
         override val description: String,
-        val isEnabled: Boolean,
+        override val isEnabled: Boolean,
         override val isLoading: Boolean = false,
         override val error: String? = null,
-    ) : FeatureUiState()
+    ) : PolicyUiState()
 
     data class ConfigurableToggle(
         override val isSupported: Boolean,
         override val title: String,
         override val featureName: String,
         override val description: String,
-        val isEnabled: Boolean,
+        override val isEnabled: Boolean,
         val configurationOptions: Map<String, Any?>,
         override val isLoading: Boolean = false,
         override val error: String? = null,
-    ) : FeatureUiState()
+    ) : PolicyUiState()
 }
