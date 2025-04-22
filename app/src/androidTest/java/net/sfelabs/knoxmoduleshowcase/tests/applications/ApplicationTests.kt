708a02db771670180b67cb44c77eef363b2a9a13
Jeff Picklyk <jpicklyk@gmail.com>
2025-04-22 11:08:21 -0400

    Provided failure messages for asserts in the ApplicationTests class.

--- app/src/androidTest/java/net/sfelabs/knoxmoduleshowcase/tests/applications/ApplicationTests.kt
+++ app/src/androidTest/java/net/sfelabs/knoxmoduleshowcase/tests/applications/ApplicationTests.kt
@@ -14,64 +14,115 @@ class ApplicationTests {
     @Test
     @TacticalSdkSuppress(minReleaseVersion = 110)
     fun checkThatKidsModeHasBeenRemoved() {
-        assertFalse(isPackageInstalled("com.sec.android.app.kidshome", pm))
-        assertFalse(isPackageInstalled("com.sec.android.app.kidsinstaller", pm))
+        assertFalse(
+            isPackageInstalled("com.sec.android.app.kidshome", pm),
+            "com.sec.android.app.kidshome is still installed but should be removed"
+        )
+        assertFalse(
+            isPackageInstalled("com.sec.android.app.kidsinstaller", pm),
+            "com.sec.android.app.kidsinstaller is still installed but should be removed"
+        )
     }
 
     @Test
     @TacticalSdkSuppress(minReleaseVersion = 130)
     fun checkThatGameLauncherHasBeenRemoved() {
-        assertFalse(isPackageInstalled("com.samsung.android.game.gamehome", pm))
-        assertFalse(isPackageInstalled("com.samsung.android.game.gametools", pm))
-        assertFalse(isPackageInstalled("com.samsung.android.game.gos", pm))
+        assertFalse(
+            isPackageInstalled("com.samsung.android.game.gamehome", pm),
+            "com.samsung.android.game.gamehome is still installed but should be removed"
+        )
+        assertFalse(
+            isPackageInstalled("com.samsung.android.game.gametools", pm),
+            "com.samsung.android.game.gametools is still installed but should be removed"
+        )
+        assertFalse(
+            isPackageInstalled("com.samsung.android.game.gos", pm),
+            "com.samsung.android.game.gos is still installed but should be removed"
+        )
     }
 
     @Test
     @TacticalSdkSuppress(minReleaseVersion = 110)
     fun checkThatFacebookHasBeenRemoved() {
-        assertFalse(isPackageInstalled("com.facebook.android", pm))
+        assertFalse(
+            isPackageInstalled("com.facebook.android", pm),
+            "com.facebook.android is still installed but should be removed"
+        )
         //preload
-        assertFalse(isPackageInstalled("com.facebook.katana", pm))
-        assertFalse(isPackageInstalled("com.facebook.appmanager", pm))
-        assertFalse(isPackageInstalled("com.facebook.system", pm))
-        assertFalse(isPackageInstalled("com.facebook.services", pm))
+        assertFalse(
+            isPackageInstalled("com.facebook.katana", pm),
+            "com.facebook.katana is still installed but should be removed"
+        )
+        assertFalse(
+            isPackageInstalled("com.facebook.appmanager", pm),
+            "com.facebook.appmanager is still installed but should be removed"
+        )
+        assertFalse(
+            isPackageInstalled("com.facebook.system", pm),
+            "com.facebook.system is still installed but should be removed"
+        )
+        assertFalse(
+            isPackageInstalled("com.facebook.services", pm),
+            "com.facebook.services is still installed but should be removed"
+        )
     }
 
     @Test
     @TacticalSdkSuppress(minReleaseVersion = 110)
     fun checkThatNetflixHasBeenRemoved() {
-        assertFalse(isPackageInstalled("com.netflix.mediaclient", pm))
+        assertFalse(
+            isPackageInstalled("com.netflix.mediaclient", pm),
+            "com.netflix.mediaclient is still installed but should be removed"
+        )
     }
 
     @Test
     @TacticalSdkSuppress(minReleaseVersion = 110)
     fun checkThatSpotifyHasBeenRemoved() {
-        assertFalse(isPackageInstalled("com.spotify.music", pm))
+        assertFalse(
+            isPackageInstalled("com.spotify.music", pm),
+            "com.spotify.music is still installed but should be removed"
+        )
     }
 
     @Test
     @TacticalSdkSuppress(minReleaseVersion = 130)
     fun checkThatLinkedInHasBeenRemoved() {
-        assertFalse(isPackageInstalled("com.linkedin.android", pm))
+        assertFalse(
+            isPackageInstalled("com.linkedin.android", pm),
+            "com.linkedin.android is still installed but should be removed"
+        )
     }
 
     @Test
     @TacticalSdkSuppress(minReleaseVersion = 130)
     fun checkThatBixbyDictationHasBeenRemoved() {
-        assertFalse(isPackageInstalled("com.samsung.android.bixby.service", pm))
+        assertFalse(
+            isPackageInstalled("com.samsung.android.bixby.service", pm),
+            "com.samsung.android.bixby.service is still installed but should be removed"
+        )
     }
 
     @Test
     @TacticalSdkSuppress(minReleaseVersion = 130)
     fun checkThatBixbyVisionHasBeenRemoved() {
-        assertFalse(isPackageInstalled("com.samsung.android.visionintelligence", pm))
-        assertFalse(isPackageInstalled("com.samsung.android.bixbyvision.framework", pm))
+        assertFalse(
+            isPackageInstalled("com.samsung.android.visionintelligence", pm),
+            "com.samsung.android.visionintelligence is still installed but should be removed"
+        )
+        assertFalse(
+            isPackageInstalled("com.samsung.android.bixbyvision.framework", pm),
+            "com.samsung.android.bixbyvision.framework is still installed but should be removed"
+        )
     }
 
     @Test
     @TacticalSdkSuppress(minReleaseVersion = 130)
     fun checkThatBixbyVoiceHasBeenRemoved() {
-        assertFalse(isPackageInstalled("com.samsung.android.bixby.agent", pm))
+        assertFalse(
+            isPackageInstalled("com.samsung.android.bixby.agent", pm),
+            "com.samsung.android.bixby.agent is still installed but should be removed"
+        )
     }
 
     // This cannot be removed as there are other components reliant on it that are required by Google
