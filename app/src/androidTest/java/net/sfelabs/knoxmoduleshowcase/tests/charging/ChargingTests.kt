0379861cb483237741f670391a48c64c959645dc
jpicklyk <5747775+jpicklyk@users.noreply.github.com>
2025-08-21 13:36:51 -0400

    Add "Disable Fast Charging" policy and tests
    This commit introduces a new policy to disable fast charging and includes corresponding use cases and tests.
    
    Key changes:
    - **New Policy:**
        - Added `DisableFastChargingPolicy` to control the fast charging setting.
    - **Use Cases:**
        - `IsFastChargingEnabledUseCase`: Checks if fast charging is currently enabled.
        - `EnableFastChargingUseCase`: Enables or disables fast charging.
    - **Tests:**
        - Added `ChargingTests` with test cases for enabling and disabling fast charging to verify the functionality of the new use cases.

---
+++ app/src/androidTest/java/net/sfelabs/knoxmoduleshowcase/tests/charging/ChargingTests.kt
@@ -0,0 +1,49 @@
+package net.sfelabs.knoxmoduleshowcase.tests.charging
+
+import androidx.test.filters.SmallTest
+import kotlinx.coroutines.test.runTest
+import net.sfelabs.knox.core.domain.usecase.model.ApiResult
+import net.sfelabs.knox_tactical.annotations.TacticalSdkSuppress
+import net.sfelabs.knox_tactical.domain.use_cases.charging.EnableFastChargingUseCase
+import net.sfelabs.knox_tactical.domain.use_cases.charging.IsFastChargingEnabledUseCase
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+@SmallTest
+@TacticalSdkSuppress
+class ChargingTests {
+
+    @Test
+    fun disableFastCharging() = runTest {
+        val getUseCase = IsFastChargingEnabledUseCase()
+        val setUseCase = EnableFastChargingUseCase()
+
+        val setResult = setUseCase(false)
+        assertTrue(
+            "API result was not successful, " +
+                    "${setResult.getErrorOrNull()}", setResult is ApiResult.Success
+        )
+        val isEnabled = getUseCase()
+        assertTrue(
+            "Fast charging is enabled!",
+            isEnabled is ApiResult.Success && !isEnabled.data
+        )
+    }
+
+    @Test
+    fun enableFastCharging() = runTest {
+        val getUseCase = IsFastChargingEnabledUseCase()
+        val setUseCase = EnableFastChargingUseCase()
+
+        val setResult = setUseCase(true)
+        assertTrue(
+            "API result was not successful, " +
+                    "${setResult.getErrorOrNull()}", setResult is ApiResult.Success
+        )
+        val isEnabled = getUseCase()
+        assertTrue(
+            "Fast charging is disabled!",
+            isEnabled is ApiResult.Success && isEnabled.data
+        )
+    }
+}
