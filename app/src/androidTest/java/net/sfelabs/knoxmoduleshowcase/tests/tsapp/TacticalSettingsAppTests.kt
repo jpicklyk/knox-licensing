2da3f8ccac7996407cb20a11a2009838a12ad1ac
jpicklyk <5747775+jpicklyk@users.noreply.github.com>
2026-02-04 14:54:55 -0500

    Updating for Android 14 pattern matching

--- app/src/androidTest/java/net/sfelabs/knoxmoduleshowcase/tests/tsapp/TacticalSettingsAppTests.kt
+++ app/src/androidTest/java/net/sfelabs/knoxmoduleshowcase/tests/tsapp/TacticalSettingsAppTests.kt
@@ -14,11 +14,13 @@ import org.junit.runner.RunWith
 class TacticalSettingsAppTests {
     private val te3Xc6pRegex = """^TP1A\.220624\.014\.G736U1UE.+_B2BF*""".toRegex()
     private val te3S23Regex = """^TP1A\.220624\.014\.S911U1UE.+_B2BF*""".toRegex()
-    private val te3TA5Regex = """^UP1A\.231005\.007\.X308UUE.+_B2BF*""".toRegex()
+    private val te3TA5A14Regex = """^UP1A\.231005\.007\.X308UUE.+_B2BF*""".toRegex()
+    private val te3Xc6pA14Regex = """^UP1A\.231005\.007\.G736U1UE.+_B2BF*""".toRegex()
+    private val te3S23A14Regex = """^UP1A\.231005\.007\.S911U1UE.+_B2BF*""".toRegex()
 
     @Test
-    @TacticalSdkSuppress(minReleaseVersion = 130)
-    fun testBuildNumberMatchesTacticalEdition3RegexFormat() {
+    @TacticalSdkSuppress(minReleaseVersion = 130, maxReleaseVersion = 139)
+    fun testBuildNumberMatchesTacticalEdition3Android13RegexFormat() {
         val buildNumber = Build.DISPLAY
 
         // Log the build number for easy viewing in test results
@@ -32,7 +34,27 @@ class TacticalSettingsAppTests {
             "Build number should match expected format",
             buildNumber.matches(te3S23Regex)
                     or buildNumber.matches(te3Xc6pRegex)
-                    or buildNumber.matches(te3TA5Regex)
         )
     }
+
+    @Test
+    @TacticalSdkSuppress(minReleaseVersion = 140, maxReleaseVersion = 149)
+    fun testBuildNumberMatchesTacticalEdition3Android14RegexFormat() {
+        val buildNumber = Build.DISPLAY
+
+        // Log the build number for easy viewing in test results
+        println("Device Build Number: $buildNumber")
+
+        // Assert that the build number is not null or empty
+        assertNotNull("Build number should not be null", buildNumber)
+        assertTrue("Build number should not be empty", buildNumber.isNotEmpty())
+
+        assertTrue(
+            "Build number should match expected format",
+            buildNumber.matches(te3S23A14Regex)
+                    or buildNumber.matches(te3Xc6pA14Regex)
+                    or buildNumber.matches(te3TA5A14Regex)
+        )
+    }
+
 }
